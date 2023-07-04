3c7e8ac10ac096182f080cf9fcc6cb3e3cae9c09
Chamundeswari-1903 <137503952+Chamundeswari-1903@users.noreply.github.com>
2023-07-05 01:23:04 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,50 @@
+let myResume = {
+    'bio' : {
+        "name" : "xyz",
+        "email": "abc@gmail.com",
+        "phone": "0123456789",
+    },
+
+     'address': {
+        "house no" : "x/y",
+        "street"  : "abc street",
+        "city" : "chennai",
+        "state" : "tamilNadu",
+        "pincode" : 600100,
+     },
+     "Higher Education": [
+        {
+          "institution": " XYZ College Of Engineering",
+          "department": "ECE",
+          "studyType": "fulltime",
+          "batch start year": 2016,
+          "batch end year": 2020,
+          "gpa": 7.5,
+        }
+      ],
+      "12 th education": [
+        {
+            "institution": "zyx HRSC",
+            "department": "Maths - Bio",
+            "batch year": 2016,
+            "percentage": 90.5,
+        }
+      ],
+      "10 th education": [
+        {
+            "institution": "xyz Matric School",
+            "batch year": 2014,
+            "percentage": 96.5,
+        }
+      ],
+      "skills": [
+        {
+            "lanuage": "Tamil,English",
+            "software skills": "Beginner in CODING",
+            "hobbby": [""],
+            "softskills":[""],
+        }
+      ]
+            
+}
+console.log(myResume);
